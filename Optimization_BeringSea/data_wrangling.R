6df697c76554894c4159cc0ea06062565ed30911
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-02-02 14:55:38 -0800

    changed to add weighting calculations

--- Optimization_BeringSea/data_wrangling.R
+++ Optimization_BeringSea/data_wrangling.R
@@ -1,55 +1,36 @@
-library(tidyr)
+#############################################
+## Data wrangling for linear programming exercise
+#############################################
+setwd("C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/Optimization_BeringSea")
 
-EBS = read.csv('G:/Oyafuso/data/data-raw/cpue_EBSshelf_all_spp.csv')
+##############################
+## Import Packages
+##############################
+library(tidyr); library(reshape2)
 
-spp = c(10110, 10130, 10210)
+##############################
+## Import EBS CPUE dataset
+##############################
+EBS = read.csv('C:/Users/Zack Oyafuso/Desktop/AK_BTS/data-raw/cpue_EBSshelf_all_spp.csv')
+# EBS = read.csv('G:/Oyafuso/data/data-raw/cpue_EBSshelf_all_spp.csv')
 
-df = subset(EBS,
-            YEAR > 1986 
+
+##############################
+## Subset species and years > 1986
+#############################
+spp = c('Arrowtooth_Flounder' = 10110, 'Flathead_Sole' = 10130, 
+        'Yellowfin_Sole' = 10210, 'Walleye_Pollock' = 21740,
+        'AK_Plaice' = 10285)
+
+df = subset(x = EBS,
+            subset = YEAR > 1986 
             &!(STATIONID %in% 
                  c('AZ0504', 'GF1918', 'GF2019', 'GF2120', 'GF2221', 'HG1918', 
                    'HG2019', 'HG2120','HG2221', 'IH1918', 'IH2019', 'IH2120', 
                    'IH2221', 'ON2524', 'ON2625', 'PO2423', 'PO2524', 'PO2625',
                    'PO2726', 'QP2423', 'QP2524', 'QP2625', 'QP2726', 'JI1918',
-                   'JI2019', 'JI2120', 'JI2221'))
+                   'JI2019', 'JI2120', 'JI2221', 'J-13'))
             & SPECIES_CODE %in% spp,  
             select = c('YEAR', 'STATIONID', 'SPECIES_CODE', 'WGTCPUE'))
 
-df2 = data.frame()
-
-for(id in sort(unique(df$STATIONID)[unique(df$STATIONID) != 'J-13'] )){
-  #for(id in unique(df$STATIONID) ){
-  temp_df = subset(df, STATIONID == id)
-  temp_df = temp_df[!duplicated(temp_df),]
-  
-  temp_df = spread(temp_df, key = SPECIES_CODE, value = WGTCPUE)
-  temp_df = apply(X = temp_df[,paste(spp)],
-                  MARGIN = 2, 
-                  FUN = function(x) (x - min(x))/diff(range(x)) )
-  temp_df[is.nan(temp_df)] = 0
-  
-  mutinfo <- multiinformation(discretize(temp_df))
-  
-  spp_covar = cov(temp_df[,paste(spp)])
-  
-  df2 = rbind(df2,
-              data.frame(
-                STATIONID = id,
-                tot_var =   sum( diag(spp_covar) ),
-                tot_var =   sum( spp_covar[upper.tri(x = spp_covar, diag = TRUE)] ),
-                tot_mean = sum(x = colMeans(temp_df[,paste(spp)])) )
-  ) 
-  
-  
-}
-
-stations = read.csv('10110_stations_.csv')
-stations = stations[stations$STATIONID != 'J-13',]
-
-df2[,c('lat', 'long')] = stations[,c('lat', 'long')]
-
-par(mfrow = c(2,1), mar = c(0,0,0,0))
-plot(lat ~ long, data = df2, cex = tot_mean^2 * 10, axes = F); box()
-plot(lat ~ long, data = df2, cex = tot_var^2 * 100, axes = F); box()
-
-write.csv(x = df2, file = 'optim_data.csv', row.names = F)
+write.csv(x = df, file = 'EBS_data_trimmed.csv', row.names = F)
