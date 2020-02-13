9a2904d5d5a1b5cb41daa09d90c0b91fc5e145d1
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-02-13 12:19:42 -0800

    modified workflow so that each model run has a specific setting

---
+++ Model_Settings.R
@@ -0,0 +1,31 @@
+###############################
+## Model Settings for VAST
+###############################
+
+setwd('C:/Users/zack.oyafuso/Work/GitHub/MS_OM_GoA/')
+
+library(VAST)
+
+# Version of VAST
+rm(list = ls())
+modelno = '1'
+Version = get_latest_version( version = NULL, package="VAST" )
+
+## Model settings
+FieldConfig = c("Omega1"=3, "Epsilon1"=3, "Omega2"=3, "Epsilon2"=3) 
+RhoConfig = c("Beta1"=0, "Beta2"=0, "Epsilon1"=0, "Epsilon2"=0) 
+OverdispersionConfig = c("Eta1"=0, "Eta2"=0)
+ObsModel = c(2,0)   
+
+##Derived Products: We also decide on which post-hoc calculations to include in the output
+Options =  c("SD_site_density"=1, 
+             "SD_site_logdensity"=0, 
+             "Calculate_Range"=0, 
+             "Calculate_evenness"=0, 
+             "Calculate_effective_area"=0, 
+             "Calculate_Cov_SE"=0, 
+             'Calculate_Synchrony'=0, 
+             'Calculate_Coherence'=0)
+
+if(!dir.exists(paste0('VAST_output', modelno))) dir.create(paste0('VAST_output', modelno))
+save.image(paste0('VAST_output', modelno, '/Model_Settings.RData'))
