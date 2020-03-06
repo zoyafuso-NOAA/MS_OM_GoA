771fe41b15de391c023ed23219411dc8f65c459c
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-03-05 18:06:55 -0800

    first init

--- Optimum_Allocation/GOA_Optimal_Allocation.R
+++ Optimum_Allocation/GOA_Optimal_Allocation.R
@@ -1,4 +1,5 @@
 
+dev.off(); rm(list = ls())
 library(VAST); library(mvtnorm); library(SamplingStrata)
 
 setwd('C:/Users/Zack Oyafuso/Google Drive/VAST_Runs/')
@@ -7,6 +8,7 @@ setwd('C:/Users/Zack Oyafuso/Google Drive/VAST_Runs/')
 VAST_model = "1b"
 load(paste0('VAST_output',VAST_model,'/VAST_MS_GoA_Run.RData'))
 load(paste0('VAST_output',VAST_model,'/Spatial_Settings.RData'))
+load("C:/Users/Zack Oyafuso/Documents/Github/MS_OM_GoA/Extrapolation_depths.RData")
 
 Opt = Save$Opt
 Report = Save$Report
@@ -18,7 +20,7 @@ Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
 
 Ests = Report$Index_gcyl[,,Years2Include,]
 
-depth_01 = Extrapolation_List$Data_Extrap$depth
+depth_01 = Extrapolation_depths$depth
 
 df = cbind(
   data.frame(Domain = "GoA",
@@ -73,8 +75,10 @@ solution3 <- optimStrata(method = "continuous",
                          framesamp = frame3,
                          iter = 50,
                          pops = 10,
+				 minnumstr=10,
                          nStrata = nstrata3,
-                         suggestions = initial_solution3)
+                         suggestions = initial_solution3,
+parallel = TRUE, cores = 6)
 
 strataStructure <- summaryStrata(solution3$framenew,
                                  solution3$aggr_strata,
@@ -82,7 +86,85 @@ strataStructure <- summaryStrata(solution3$framenew,
 strataStructure
 
 colors = c('black', 'red', 'green', 'blue', 'orange', 'gold', 'brown')
-par(mar = c(0,0,0,0))
 plot(Spatial_List$loc_g, pch = 16, cex = 0.5, 
      col = colors[solution3$indices[,'X1']])
 expected_CV(strata = solution3$aggr_strata)
+
+
+## Spatial Method
+index_means =apply(X = Ests, MARGIN = 1:2, FUN = mean)
+index_vars = apply(X = Ests, MARGIN = 1:2, FUN = var)
+colnames(index_means) = paste0(gsub(x = Save$Spp, pattern = ' ', replacement = '_'), '_mean')
+colnames(index_vars) = paste0(gsub(x = Save$Spp, pattern = ' ', replacement = '_'), '_var')
+
+df = cbind(
+  data.frame(Domain = "GoA",
+		dom1 = 1,
+		 depth = Extrapolation_depths$depth,
+             x = 1:TmbData$n_g,
+		lon = Extrapolation_depths$E_km,
+		lat = Extrapolation_depths$N_km),
+  index_means, index_vars
+  
+)
+
+df <- as.data.frame(df)
+head(df)
+
+frame <- buildFrameSpatial(df=df,
+                      id="x",
+                      X="depth",
+                      Y=paste0(gsub(x = Save$Spp, pattern = ' ', replacement = '_'), '_mean'),
+                      variance=paste0(gsub(x = Save$Spp, pattern = ' ', replacement = '_'), '_var'),
+                      lon="lon",
+                      lat="lat",
+                      domainvalue = "dom1")
+
+cv <- as.data.frame(list(DOM=rep("DOM1",1),
+                         CV1=rep(0.1,1),
+                         CV2=rep(0.1,1),
+CV3=rep(0.1,1),
+CV4=rep(0.1,1),
+                         domainvalue=c(1:1) ))
+
+init_sol <- KmeansSolution2(frame=frame,
+                             errors=cv,
+                             maxclusters = 10)  
+
+nstrata <- tapply(init_sol$suggestions,
+                   init_sol$domainvalue,
+                   FUN=function(x) length(unique(x)))
+nstrata
+initial_solution <- prepareSuggestion(init_sol,frame,nstrata)
+
+set.seed(1234)
+solution <- optimStrata (
+  method = "spatial",
+  errors=cv, 
+  framesamp=frame,
+  iter = 15,
+  pops = 10,
+  nStrata = 10,
+  fitting = rep(1, 4),
+  range = rep(1, 4),
+  kappa=1,
+  writeFiles = FALSE,
+  showPlot = TRUE)
+
+plot(Extrapolation_depths[,c("Lon", 'Lat')], pch = '.', cex = 1,
+col = heat.colors(4)[cut(frame$Y1, breaks = quantile(frame$Y1))] )
+
+strataStructure <- summaryStrata(solution$framenew,
+                                 solution$aggr_strata,
+                                 progress=FALSE)
+strataStructure
+
+colors = c('black', 'red', 'green', 'blue', 'orange', 'gold', 'brown', 'yellow')
+plot(Spatial_List$loc_g, pch = 16, cex = 0.5, 
+     col = colors[solution$indices[,'X1']])
+expected_CV(strata = solution$aggr_strata)
+
+save(list=ls()[!ls() %in% c('Extrapolation_List', 'gulf_of_alaska_grid', 'Ests', 'df',
+					'Obj', 'Opt', 'Save', 'Report', 'Data_Geostat', 
+					"Extrapolation_depths", 'Spatial_List', 'TmbData') ],
+file = paste0("C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/Optimum_Allocation/test.RData"))
