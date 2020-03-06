102796c3abfee43457bcf242a72f65aa1f23cfbc
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-03-05 21:12:54 -0800

    added parallel feature

--- Optimum_Allocation/GOA_Optimal_Allocation.R
+++ Optimum_Allocation/GOA_Optimal_Allocation.R
@@ -10,7 +10,7 @@ library(VAST); library(mvtnorm); library(SamplingStrata)
 setwd('C:/Users/Zack Oyafuso/Google Drive/VAST_Runs/')
 # setwd('C:/Users/zack.oyafuso/Work/GitHub/MS_OM_GoA/')
 
-VAST_model = "1b"
+VAST_model = "2b"
 load(paste0('VAST_output',VAST_model,'/VAST_MS_GoA_Run.RData'))
 load(paste0('VAST_output',VAST_model,'/Spatial_Settings.RData'))
 load("C:/Users/Zack Oyafuso/Documents/Github/MS_OM_GoA/Extrapolation_depths.RData")
@@ -73,36 +73,44 @@ settings = expand.grid(pops = c(25,50,100),
 library(foreach)
 initial_solution <- prepareSuggestion(init_sol,frame,nstrata)
 
+rm(list  = ls()[!ls() %in% c('settings', 'cv', 'frame', 
+                             'nstrata', 'initial_solution', 
+                             'modelno') ])
+
 cl <- parallel::makeCluster(6)
 doParallel::registerDoParallel(cl)
 
-foreach(i = 1:nrow(settings)) %do% {
-  library(SamplingStrata)
-  wd = paste0("C:/Users/Zack Oyafuso/Documents/",
-              "GitHub/MS_OM_GoA/Optimum_Allocation/tuning/",
-              'pop_', settings$pops[i], '_',
-              'minnumstr_', settings$minnumstr[i], '_',
-              'mutchange_', settings$mut_change[i], '_',
-              'elitism_rate_', settings$elitism_rate[i], '.RData')
-  
-  set.seed(1234 + i)
-  solution <- optimStrata(method = "continuous",
-                          errors = cv, 
-                          framesamp = frame,
-                          iter = 50,
-                          pops = settings$pops[i],
-                          minnumstr=settings$minnumstr[i],
-                          mut_chance = settings$mut_change[i],
-                          nStrata = nstrata,
-                          suggestions = initial_solution,
-                          showPlot = T,
-                          parallel = F)
-  strataStructure <- summaryStrata(solution$framenew,
-                                   solution$aggr_strata,
-                                   progress=FALSE)
-  
-  save(list=c('strataStructure', 'solution'), file = wd)
-}
+foreach(i = 1:nrow(settings), 
+        .packages = 'SamplingStrata',
+        .export = c('settings', 'cv', 'frame', 
+                    'nstrata', 'initial_solution', 'modelno') ) %do% 
+  {
+    library(SamplingStrata)
+    wd = paste0("C:/Users/Zack Oyafuso/Documents/",
+                "GitHub/MS_OM_GoA/Optimum_Allocation/model_", modelno, "/",
+                'pop_', settings$pops[i], '_',
+                'minnumstr_', settings$minnumstr[i], '_',
+                'mutchange_', settings$mut_change[i], '_',
+                'elitism_rate_', settings$elitism_rate[i], '.RData')
+    
+    set.seed(1234 + i)
+    solution <- optimStrata(method = "continuous",
+                            errors = cv, 
+                            framesamp = frame,
+                            iter = 50,
+                            pops = settings$pops[i],
+                            minnumstr=settings$minnumstr[i],
+                            mut_chance = settings$mut_change[i],
+                            nStrata = nstrata,
+                            suggestions = initial_solution,
+                            showPlot = T,
+                            parallel = F)
+    strataStructure <- summaryStrata(solution$framenew,
+                                     solution$aggr_strata,
+                                     progress=FALSE)
+    
+    save(list=c('strataStructure', 'solution'), file = wd)
+  }
 
 
 
---
+++ Optimum_Allocation/GOA_Optimal_Allocation_results.R
@@ -0,0 +1,42 @@
+library(raster); library(RColorBrewer)
+
+setwd('C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/Optimum_Allocation/')
+load('../Extrapolation_depths.RData')
+
+settings = results = expand.grid(pops = c(25,50,100),
+                                 minnumstr = c(10, 20),
+                                 mut_change = c(0.01, 0.05, 0.1),
+                                 elitism_rate = c(0.1, 0.2, 0.5))
+modelno = '1b'
+
+for(i in 1:nrow(settings)){
+  wd = paste0("model_", modelno, "/",
+              'pop_', settings$pops[i], '_',
+              'minnumstr_', settings$minnumstr[i], '_',
+              'mutchange_', settings$mut_change[i], '_',
+              'elitism_rate_', settings$elitism_rate[i], '.RData')
+  
+  load(wd)
+  settings$n[i] = sum( strataStructure$Allocation ) 
+}
+
+winner = which.min(settings$n)
+wd = paste0("model_", modelno, "/",
+            'pop_', settings$pops[winner], '_',
+            'minnumstr_', settings$minnumstr[winner], '_',
+            'mutchange_', settings$mut_change[winner], '_',
+            'elitism_rate_', settings$elitism_rate[winner], '.RData')
+
+load(wd)
+
+ns = sum( substr(x = colnames(solution$framenew), start = 1, stop = 1) == 'Y')
+goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km', 'N_km')], 
+                             data = cbind(solution$framenew[,paste0('Y',1:ns)],
+                                        X1 = solution$indices$X1) )
+# goa = SpatialPoints(coords = Extrapolation_depths[,c('E_km', 'N_km')] )
+goa_ras = raster(goa, resolution = 5)
+goa_ras =rasterize(x = goa, y = goa_ras, field = 'X1')
+
+par(mar = c(0,0,0,0))
+nstrata = nrow(strataStructure)
+plot(goa_ras, col = brewer.pal(n = nstrata, name= 'Paired'), legend = F )
