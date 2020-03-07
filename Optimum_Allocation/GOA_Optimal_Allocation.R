42b00bb47e65e091e4a8c21d5794386aee382fe3
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-03-06 20:41:38 -0800

    cleaned up some stuff

--- Optimum_Allocation/GOA_Optimal_Allocation.R
+++ Optimum_Allocation/GOA_Optimal_Allocation.R
@@ -23,26 +23,7 @@ df = cbind(
              depth = Extrapolation_depths$depth),
   apply(X = Save$Report$Index_gcyl[,,Years2Include,], MARGIN = 1:2, FUN = mean)
 )
-
-
 names(df)[-(1:3)] = gsub(x = Save$Spp, pattern = ' ', replacement = '_')
-
-frame1 <- buildFrameDF(df = df,
-                       id = "x",
-                       X = c("depth"),
-                       Y = gsub(x = Save$Spp, pattern = ' ', replacement = '_'),
-                       domainvalue = "Domain")
-strata1 <- buildStrataDF(frame1, progress=F)
-
-
-
-
-# checkInput(errors = checkInput(errors = cv, 
-#                                strata = strata1, 
-#                                sampframe = frame1))
-# 
-# allocation <- bethel(strata1,cv[1,])
-# sum(allocation)
 
 frame <- buildFrameDF(df = df,
                       id = "x",
@@ -51,10 +32,10 @@ frame <- buildFrameDF(df = df,
                       domainvalue = "Domain")
 
 #Settings for optimizer
-settings = results = expand.grid(cv = c(0.1, 0.2, 0.3),
-                                 nstrata = c(5,10),
-                                 mut_change = c(0.01, 0.1, 0.5),
-                                 elitism_rate = c(0.1, 0.2, 0.5))
+settings = expand.grid(cv = c(0.1, 0.2, 0.3),
+                       nstrata = c(5,10),
+                       mut_change = c(0.01, 0.1, 0.5),
+                       elitism_rate = c(0.1, 0.2, 0.5))
 
 ns = Save$TmbData$n_c
 
@@ -72,27 +53,14 @@ foreach(i = 1:nrow(settings),
                 "GitHub/MS_OM_GoA/Optimum_Allocation/",
                 "model_", modelno, "/",
                 'cv_', settings$cv[i], '_', 
-                'pop_', settings$pops[i], '_',
-                'minnumstr_', settings$minnumstr[i], '_',
-                'mutchange_', settings$mut_change[i], '_',
+                'nstrata_', settings$nstrata[i], '_',
+                'mut_change_', settings$mut_change[i], '_',
                 'elitism_rate_', settings$elitism_rate[i], '.RData')
     
     cv = list()
-    for(i in 1:ns) cv[[paste0('CV', i)]] = 0.3 #settings$cv[i]
-    cv[['DOM']] = 'GoA'; cv[['domainvalue']]=1
+    for(i in 1:ns) cv[[paste0('CV', i)]] = settings$cv[i]
+    cv[['DOM']] = 'GoA'; cv[['domainvalue']] = 1
     cv <- as.data.frame(cv)
-    # cv
-    
-    init_sol <- KmeansSolution2(frame=frame,
-                                errors=cv,
-                                maxclusters = 100)  
-    
-    nstrata <- tapply(init_sol$suggestions,
-                      init_sol$domainvalue,
-                      FUN=function(x) length(unique(x)))
-    # nstrata
-    
-    initial_solution <- prepareSuggestion(init_sol,frame,nstrata)
     
     set.seed(1234 + i)
     solution <- optimStrata(method = "continuous",
@@ -100,11 +68,12 @@ foreach(i = 1:nrow(settings),
                             framesamp = frame,
                             iter = 50,
                             pops = 100,
+                            elitism_rate = settings$elitism_rate[i],
                             mut_chance = settings$mut_change[i],
-                            nStrata = 5,
-                            # suggestions = initial_solution,
-                            showPlot = T,
+                            nStrata = settings$nstrata[i],
+                            showPlot = F,
                             parallel = F)
+    
     strataStructure <- summaryStrata(solution$framenew,
                                      solution$aggr_strata,
                                      progress=FALSE)
