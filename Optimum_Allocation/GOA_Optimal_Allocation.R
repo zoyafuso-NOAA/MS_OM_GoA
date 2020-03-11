93729ede793fc3950d7324e1f64317852f6f6372
Zack Oyafuso <zackoyafuso@Zacks-MacBook-Air.local>
2020-03-11 16:30:46 -0700

    solution map

--- Optimum_Allocation/GOA_Optimal_Allocation.R
+++ Optimum_Allocation/GOA_Optimal_Allocation.R
@@ -5,9 +5,11 @@
 
 rm(list = ls())
 library(VAST); library(mvtnorm); library(SamplingStrata); library(sp)
+library(RColorBrewer); library(raster)
 
 VAST_model = "6c"
-setwd('C:/Users/Zack Oyafuso/Google Drive/VAST_Runs/')
+setwd('/Users/zackoyafuso/Google Drive/VAST_Runs/')
+#setwd('C:/Users/Zack Oyafuso/Google Drive/VAST_Runs/')
 # setwd('C:/Users/zack.oyafuso/Work/GitHub/MS_OM_GoA/')
 
 wd = paste0("C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/",
@@ -17,7 +19,9 @@ if(!dir.exists(wd)) dir.create(wd)
 
 load(paste0('VAST_output',VAST_model,'/VAST_MS_GoA_Run.RData'))
 load(paste0('VAST_output',VAST_model,'/Spatial_Settings.RData'))
-load("C:/Users/Zack Oyafuso/Documents/Github/MS_OM_GoA/Extrapolation_depths.RData")
+
+load('/Users/zackoyafuso/Documents/GitHub/MS_OM_GoA/Extrapolation_depths.RData')
+#load("C:/Users/Zack Oyafuso/Documents/Github/MS_OM_GoA/Extrapolation_depths.RData")
 
 Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
 Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
@@ -49,7 +53,7 @@ settings = expand.grid(cv = c(0.3),
 ns = Save$TmbData$n_c
 domains = unique(df$Domain)
 ndom = length(unique(frame$domainvalue))
-  
+
 # rm(list  = ls()[!ls() %in% c('settings', 'frame', 'VAST_model', 'ns') ])
 
 # for(i in 1:nrow(settings)){
@@ -60,52 +64,95 @@ ndom = length(unique(frame$domainvalue))
 #               'cv_', settings$cv[i], '_', 
 #               'mut_change_', settings$mut_change[i], '_',
 #               'elitism_rate_', settings$elitism_rate[i], '.RData')
-  i=1
-  cv = list()
-  for(spp in 1:ns) cv[[paste0('CV', spp)]] = rep(settings$cv[i], ndom)
-  cv[['DOM']] = levels(domains)
-  cv[['domainvalue']] = as.numeric(domains)
-  cv <- as.data.frame(cv)
-  
-  set.seed(1234 + i)
-  solution <- optimStrata(method = "continuous",
-                          errors = cv, 
-                          framesamp = frame,
-                          iter = 20,
-                          pops = 50,
-                          elitism_rate = settings$elitism_rate[i],
-                          mut_chance = settings$mut_change[i],
-                          nStrata = rep(3, ndom),
-                          showPlot = T,
-                          parallel = T)
-  
-  strataStructure <- summaryStrata(solution$framenew,
-                                   solution$aggr_strata,
-                                   progress=FALSE)
-  
+i=1
+cv = list()
+for(spp in 1:ns) cv[[paste0('CV', spp)]] = rep(settings$cv[i], ndom)
+cv[['DOM']] = levels(domains)
+cv[['domainvalue']] = as.numeric(domains)
+cv <- as.data.frame(cv)
+
+set.seed(1234 + i)
+solution <- optimStrata(method = "continuous",
+                        errors = cv, 
+                        framesamp = frame,
+                        iter = 20,
+                        pops = 50,
+                        elitism_rate = settings$elitism_rate[i],
+                        mut_chance = settings$mut_change[i],
+                        nStrata = rep(3, ndom),
+                        showPlot = T,
+                        parallel = T)
+
+strataStructure <- summaryStrata(solution$framenew,
+                                 solution$aggr_strata,
+                                 progress=FALSE)
+
 #   save(list=c('strataStructure', 'solution'), file = wd)
 # }
 
-  library(RColorBrewer); library(raster)
-  load('C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/Extrapolation_depths.RData')
-  
-  goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km', 'N_km')], 
-                               data = cbind(solution$framenew[,paste0('Y',1:ns)],
-                                            X1 = solution$indices$X1,
-                                            domain = df$Domain) )
-  # goa = SpatialPoints(coords = Extrapolation_depths[,c('E_km', 'N_km')] )
-  goa_ras = raster(goa, resolution = 5)
-  goa_ras =rasterize(x = goa, y = goa_ras, field = 'X1')
-  
-  par(mar = c(0,0,0,0))
-  nstrata = nrow(strataStructure)
-  plot(goa_ras, col = rev(brewer.pal(n = 3, name= 'Spectral')), legend = T )
-  
-  
-  # plot(N_km ~ E_km, data = Extrapolation_depths, pch = '.', cex = 2,
-       # col = brewer.pal(n = 5, name = 'Spectral')[frame$domainvalue])
-  
-  expected_CV(solution$aggr_strata)
-  sum(strataStructure$Allocation)
+load('C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/Extrapolation_depths.RData')
 
+goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km', 'N_km')], 
+                             data = cbind(solution$framenew[,paste0('Y',1:ns)],
+                                          X1 = solution$framenew$STRATO,
+                                          domain = df$Domain) )
+
+{tiff('/Users/zackoyafuso/Documents/GitHub/MS_OM_GoA/Optimum_Allocation/solution_map.tiff', res = 200, width = 190, height = 200, units = 'mm',
+      compression = 'lzw')
+  par(mfrow = c(3,2), oma = rep(1,4), family = 'serif' )
   
+  #Plot by management region
+  for(idom in 1:ndom){
+    goa_ras = raster(subset(goa, domain == levels(goa$domain)[idom] ), resolution = 5)
+    goa_ras =rasterize(x = goa, y = goa_ras, field = 'X1')
+    
+    colorRamp = colorRampPalette(
+      
+      list(c('yellow', 'red', 'brown'),
+           c('grey', 'yellow', 'gold'),
+           c('lawngreen', 'green', 'darkgreen'),
+           c('grey', 'blue', 'darkblue'),
+           c('grey', 'purple', 'darkorchid4'))[[idom]]
+    )(10)
+    
+    par(mar = c(0,0,0,0))
+    plot(goa_ras, 
+         col = colorRamp[c(10,7,3)], 
+         axes = F, legend = F )
+    
+    legend_label = c()
+    temp_df = subset(strataStructure, Domain == idom)
+    
+    for(istratum in 1:nrow(temp_df)){
+      legend_label = c( legend_label, paste0( 'Str ', istratum, ': ', temp_df$Population[istratum],
+                                              ' units--', temp_df$Allocation[istratum], ' allocated'))
+    }
+    
+    xrange = diff(par()$usr[1:2])
+    yrange = diff(par()$usr[3:4])
+    legend(x = par()$usr[1]+xrange*c(0.1,0.1,0.45,0.25,0.45)[idom],
+           y = par()$usr[3]+yrange*c(0.975,0.975,0.275,0.98,0.975)[idom],
+           legend = legend_label, bty = 'n', pt.cex = 3,
+           title = paste0('Region ', idom),
+           col = colorRamp[c(10,7,3)], pch = 15, cex = 1.)
+  }
+  
+  par( mar = c(0,0,0,0) )
+  plot(N_km ~ E_km, data = Extrapolation_depths, pch = '.', cex = 2,
+       col = brewer.pal(n = 10, name = 'Spectral')[c(1,5,7,9,10)][frame$domainvalue],
+       axes = F, asp=1)
+  text(x = tapply(Extrapolation_depths$E_km, frame$domainvalue, mean),
+       y = tapply(Extrapolation_depths$N_km, frame$domainvalue, mean),
+       font = 2, cex = 2)
+  
+  xrange = diff(par()$usr[1:2])
+  yrange = diff(par()$usr[3:4])
+  text(x = par()$usr[1]+xrange*0.35,
+       y = par()$usr[3]+yrange*0.85,
+       paste0('Optimal Sample Size: ', sum(strataStructure$Allocation), '\n',
+              'CV constraint: ', unique(cv$CV1)*100, '%'),
+       cex = 2)
+  
+  #expected_CV(solution$aggr_strata)
+  dev.off()
+}
