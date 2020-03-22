8bb0e1fcaedfeda0b0eba2a51f8b409ca548ea78
Zack Oyafuso <zackoyafuso@Zacks-MacBook-Air.local>
2020-03-21 23:01:17 -0700

    updated plots

--- Optimum_Allocation/sol_by_domain.R
+++ Optimum_Allocation/sol_by_domain.R
@@ -2,90 +2,105 @@ library(raster); library(RColorBrewer); library(SamplingStrata)
 
 which_machine = c('Zack_MAC' = 1)
 result_wd = c('/Users/zackoyafuso/Documents/GitHub/MS_OM_GoA/Optimum_Allocation/', 
-              'C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/Optimum_Allocation/')[2]
+              'C:/Users/Zack Oyafuso/Documents/GitHub/MS_OM_GoA/Optimum_Allocation/')[which_machine]
 
 setwd(result_wd)
 load('../Extrapolation_depths.RData')
-VAST_model = '6d'
+VAST_model = '6g'
 load(paste0('model_', VAST_model, '/optimization.RData'))
 
 res_df = as.data.frame(res_df)
 sample_sizes = sapply(strata_list, FUN = function(x) sum(x$Allocation))
 
-winner = which.min(sample_sizes)
-
+winner = which.min(sample_sizes[settings$cv == 0.15])
 ns = 15
-ndom = 5
-
 goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km', 'N_km')], 
-                             data = data.frame(X1=res_df[,paste0('V',winner+2)],
-                                               domainvalue = res_df$domainvalue) )
+                             data = data.frame(X1=res_df[,paste0('V',
+                                                                 winner+2)]) )
 
-{tiff('solution_map.tiff', res = 200, width = 190, height = 200, units = 'mm',
-      compression = 'lzw')
-  par(mfrow = c(3,2), oma = rep(1,4), family = 'serif' )
+{tiff(paste0('model_', VAST_model, '/solution_map.tiff'), 
+      res = 200, width = 8, height = 6, units = 'in', compression = 'lzw')
+  par(mfrow = c(1,1), oma = rep(1,4), family = 'serif' )
   
-  #Plot by management region
-  for(idom in 1:ndom){
-    goa_ras = raster(subset(goa, domainvalue == idom ), 
-                     resolution = 5)
-    goa_ras =rasterize(x = subset(goa, domainvalue == idom ), 
-                       y = goa_ras, field = 'X1')
-    
-    colorRamp = colorRampPalette(
-      
-      list(c('grey', 'yellow', 'red', 'brown'),
-           c('grey', 'yellow', 'gold'),
-           c('grey', 'lawngreen', 'green', 'darkgreen'),
-           c('grey', 'cyan','blue', 'darkblue'),
-           c('grey', 'purple', 'darkorchid4'))[[idom]]
-    )(10)
-    
-    temp_df = subset(strata_list[[winner]], Domain == idom)
-    nstrata = nrow(temp_df)
-    
-    par(mar = c(0,0,0,0))
-    plot(goa_ras, asp = 1, legend = F,
-         col = colorRamp[c(10,7,3,1)[1:nstrata]], 
-         axes = F)
-    #box()
-    
-    legend_label = c()
-    
-    
-    for(istratum in 1:nrow(temp_df)){
-      legend_label = c( legend_label, 
-                        paste0(temp_df$Population[istratum],
-                               ' units--', temp_df$Allocation[istratum], 
-                               ' allocated (', temp_df$Lower_X1[istratum], '-',
-                               temp_df$Upper_X1[istratum], ' m)'))
-    }
-    
-    xrange = diff(par()$usr[1:2])
-    yrange = diff(par()$usr[3:4])
-    legend(x = par()$usr[1]+xrange*c(0.1,0.1,0.45,0.25,0.45)[idom],
-           y = par()$usr[3]+yrange*c(0.975,0.975,0.275,0.98,0.975)[idom],
-           legend = legend_label, bty = 'n', pt.cex = 3,
-           title = paste0('Region ', idom),
-           col = colorRamp[c(10,7,3,1)[1:nstrata]], pch = 15, cex = 0.9)
-  }
+  goa_ras = raster(goa, resolution = 5)
+  goa_ras = rasterize(x = goa, y = goa_ras, field = 'X1')
   
-  par( mar = c(0,0,0,0) )
-  plot(N_km ~ E_km, data = Extrapolation_depths, pch = '.', cex = 2,
-       col = brewer.pal(n = 10, name = 'Spectral')[c(1,5,7,9,10)][goa$domainvalue],
-       axes = F, asp=1)
-  text(x = tapply(Extrapolation_depths$E_km, goa$domainvalue, mean),
-       y = tapply(Extrapolation_depths$N_km, goa$domainvalue, mean),
-       font = 2, cex = 2)
+  temp_df = strata_list[[winner]]
+  nstrata = nrow(temp_df)
+  
+  par(mar = c(0,0,0,0))
+  image(goa_ras, asp = 1, axes = F,
+        col = brewer.pal(n = nstrata, name = 'Paired'))
   
   xrange = diff(par()$usr[1:2])
   yrange = diff(par()$usr[3:4])
   text(x = par()$usr[1]+xrange*0.35,
        y = par()$usr[3]+yrange*0.85,
        paste0('Optimal Sample Size: ', 
-              sum(strata_list[[winner]]$Allocation), '\n',
-              'CV constraint: ', unique(0.3)*100, '%'),
-       cex = 2)
+              sum(temp_df$Allocation), '\n',
+              'CV constraint: ', settings$cv[winner]*100, '%'),
+       cex = 1.5)
   
+  dev.off()
+}
+
+temp_df = strata_list[[winner]]
+nstrata = nrow(temp_df)
+depth_cuts = unique(ceiling(sort(c(temp_df$Lower_X1, temp_df$Upper_X1))))
+lon_cuts = unique(ceiling(sort(c(temp_df$Lower_X2, temp_df$Upper_X2))))
+
+matrix_space = matrix(data = nstrata + 1,
+                      nrow = length(depth_cuts)-1, 
+                      ncol = length(lon_cuts)-1,
+                      dimnames = list(
+                        paste0(depth_cuts[1:(length(depth_cuts)-1)], '-',
+                               depth_cuts[2:length(depth_cuts)]),
+                        paste0(lon_cuts[1:(length(lon_cuts)-1)], '-',
+                               lon_cuts[2:length(lon_cuts)]) ) )
+
+for(istrata in nstrata:1){
+  depth_bounds = ceiling(unlist(temp_df[istrata,c('Lower_X1','Upper_X1')]))
+  col_idx = c(which(depth_cuts[1:(length(depth_cuts)-1)] == depth_bounds[1]),
+              which(depth_cuts[2:length(depth_cuts)] == depth_bounds[2] ) )
+  
+  lon_bounds = ceiling(unlist(temp_df[istrata,c('Lower_X2','Upper_X2')]))
+  row_idx = c(which(lon_cuts[1:(length(lon_cuts)-1)] == lon_bounds[1]),
+              which(lon_cuts[2:length(lon_cuts)] == lon_bounds[2] ))
+  
+  for(j in seq(row_idx[1],row_idx[2])){
+    for(i in seq(col_idx[1],col_idx[2]) ){
+      matrix_space[i,j] = ifelse(matrix_space[i,j] > istrata, 
+                                 istrata, 
+                                 matrix_space[i,j])
+    }
+  }
+}
+
+for(j in 1:(length(lon_cuts)-1) ){
+  for(i in 1:(length(depth_cuts)-1)){
+    matrix_space[i,j] = ifelse(any(frame$X1 > depth_cuts[i] & 
+                                     frame$X1 < depth_cuts[i+1] &
+                                     frame$X2 > lon_cuts[j] & 
+                                     frame$X2 < lon_cuts[j+1]), 
+                               matrix_space[i,j], 
+                               nstrata+1)
+  }
+}
+
+{tiff(paste0('model_', VAST_model, '/solution_space.tiff'), 
+      res = 200, width = 5, height = 3.65, units = 'in', compression = 'lzw')
+  par(mar = c(4,4,1,1), oma = c(0,0.5,1,0.5))
+  sol_ras = raster(matrix_space, xmn = 0, xmx = ncol(matrix_space),
+                   ymn = 0, ymx = nrow(matrix_space) )
+  image(sol_ras, axes = F, asp = 1, xlab = 'Eastings (km)', ylab = 'Depth (m)', 
+        col = c(brewer.pal(n = nstrata, 'Paired'), 'black'))
+  axis(side = 1, at = 0:(sol_ras@extent[2]) ,
+       labels = lon_cuts, las = 1, cex.axis = 0.6)
+  axis(side = 2, at = (sol_ras@extent[4]):0 , 
+       labels = depth_cuts, las = 1)
+  
+  abline(h = 0:length(depth_cuts), v = 0:length(lon_cuts))
+  legend(x = 0.25, y = 8.25, legend = 1:nstrata, horiz = T, xpd = NA, 
+         bty = 'n', fill = brewer.pal(n = nstrata, 'Paired'))
   dev.off()
 }
