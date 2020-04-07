b52f3c184bf953b36b3fd1599b854a0f8a4c6362
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-04-07 11:16:57 -0700

    updated

--- Optimum_Allocation/GOA_Optimal_Allocation_continuous_spatiotemporarl_2vars.R
+++ Optimum_Allocation/GOA_Optimal_Allocation_continuous_spatiotemporarl_2vars.R
@@ -1,7 +1,6 @@
-#################################
-## Parallelize the Optimization Process
-## Method == "continous"
-#################################
+################################################
+## Optimization: Lowest CV for a given sample size
+################################################
 rm(list = ls())
 
 ###############################
@@ -12,11 +11,10 @@ library(raster)
 library(memoise); library(doParallel); library(foreach); library(iterators); 
 library(parallel); library(pbapply); library(formattable)
 
-
 ###############################
 ## Set up directories
 ###############################
-which_machine = c('Zack_MAC'=1, 'Zack_PC' =2, 'Zack_GI_PC'=3, 'VM' = 4)[2]
+which_machine = c('Zack_MAC'=1, 'Zack_PC' =2, 'Zack_GI_PC'=3, 'VM' = 4)[4]
 
 SamplingStrata_dir = paste0(c('', 
                               'C:/Users/Zack Oyafuso',
@@ -51,143 +49,96 @@ output_wd = c(paste0('/Users/zackoyafuso/Documents/GitHub/MS_OM_GoA/',
 for(ifile in dir(SamplingStrata_dir, full.names = T)) source(ifile)
 source(paste0(github_dir, '/buildStrataDF_Zack.R'))
 
-#########################
-## Load VAST products
-#########################
-load(paste0(VAST_dir, '/VAST_MS_GoA_Run.RData'))
-load(paste0(VAST_dir, '/Spatial_Settings.RData'))
-load(paste0(dirname(github_dir), '/Extrapolation_depths.RData'))
-
-#########################
-## Index years that had data
-#########################
-Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
-Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
-NTime = length(Years2Include)
-
-##########################
-## Create the data inputs to SamplingStrata
-##########################
-df = df_raw = NULL
-
-df = cbind(
-  data.frame(Domain = 1,
-             x = 1:Save$TmbData$n_g,
-             lat = Extrapolation_depths$N_km,
-             lon = Extrapolation_depths$E_km - min(Extrapolation_depths$E_km),
-             depth = Extrapolation_depths$depth),
-  apply(X=Save$Report$Index_gcyl[,,Years2Include,], MARGIN = 1:2, FUN = mean ) )
-
-names(df)[-(1:5)] = gsub(x = Save$Spp, pattern = ' ', replacement = '_')
-
-frame <- buildFrameDF(df = df,
-                      id = "x",
-                      X = c("depth", 'lon'),
-                      Y = gsub(x = Save$Spp, pattern = ' ', replacement = '_'),
-                      domainvalue = "Domain")
-
-for(iT in 1:NTime){
-  df_raw = rbind(df_raw, cbind(
-    data.frame(Domain = 1,
-               x = 1:Save$TmbData$n_g,
-               year = iT,
-               lat = Extrapolation_depths$N_km,
-               lon = Extrapolation_depths$E_km - min(Extrapolation_depths$E_km),
-               depth = Extrapolation_depths$depth),
-    Save$Report$Index_gcyl[,,Years2Include[iT],] )
-  )
-}
-names(df_raw)[-(1:6)] = gsub(x = Save$Spp, pattern = ' ', replacement = '_')
-frame_raw <- buildFrameDF(df = df_raw,
-                          id = "x",
-                          X = c("depth", 'lon'),
-                          Y = gsub(x = Save$Spp, pattern = ' ', replacement = '_'),
-                          domainvalue = "Domain")
+###########################
+## Load Data
+###########################
+load(paste0(output_wd, '/optimization_data_model_', 
+            VAST_model, '.RData'))
 
 ############################
 ## Settings for optimizer
 ############################
-settings = rbind(expand.grid(cv = c(0.2, 0.15),
-                             mut_change = c(0.1, 0.01),
-                             elitism_rate = c(0.2, 0.1),
-                             nstata = c(5,7,10),
-                             iter = 1:10),
-                 expand.grid(cv = c(0.2, 0.15),
-                             mut_change = 0.1,
-                             elitism_rate = 0.1,
-                             nstata = c(6,8,11:20),
-                             iter = 1:10),
-                 expand.grid(cv = c(0.2, 0.15),
-                             mut_change = 0.1,
-                             elitism_rate = 0.1,
-                             nstata = 9,
-                             iter = 1:10),
-                 expand.grid(cv = c(0.2, 0.15),
-                             mut_change = 0.1,
-                             elitism_rate = 0.1,
-                             nstata = 5:20,
-                             iter = 11:20),
-                 expand.grid(cv = c(0.2, 0.15),
-                             mut_change = 0.1,
-                             elitism_rate = 0.1,
-                             nstata = seq(25, 60, 5),
-                             iter = 1:5)
-                 
-)
+nstrata = unlist(list('Zack_MAC'=NA, 
+               'Zack_PC' =c(5,7,10, 15), 
+               'Zack_GI_PC'= c(20,25,30), 
+               'VM' = c(40,50,60))[which_machine])
 
-ns = Save$TmbData$n_c
+nstrata = 40
 
-rm(list = c('Save', 'Spatial_List', 'spp_df', 'strata.limits', 'fine_scale',
-            'Method', 'modelno', 'n_x', 'which_spp', 'Year_Set', 
-            'Years2Include', 'Data_Geostat', 'df', 'Extrapolation_List',
-            'gulf_of_alaska_grid', 'ifile', 'iT', 'df_raw'))
-
-res_df = as.matrix(frame[,c('id', 'domainvalue')])
+settings = data.frame()
+res_df = data.frame(id = 1:nrow(frame))
 strata_list = list()
 
-iter_range = unlist(list('Zack_MAC'= NA, 'Zack_PC' = 616:650,
-                         'Zack_GI_PC'=321:400, 'VM' = 401:480)[which_machine])
 
-for(ii in as.integer(iter_range)){
+for(istrata in nstrata){
   
-  cv = list()
-  for(spp in 1:ns) cv[[paste0('CV', spp)]] = settings$cv[ii]
-  cv[['DOM']] = 1
-  cv[['domainvalue']] = 1
-  cv <- as.data.frame(cv)
+  iseed = istrata
+  current_n = 10000
+  current_CV = 0.15
   
-  set.seed(1234 + ii)
-  solution <- optimStrata(method = "continuous",
-                          errors = cv, 
-                          framesamp = frame,
-                          iter = 50,
-                          pops = 30,
-                          elitism_rate = settings$elitism_rate[ii],
-                          mut_chance = settings$mut_change[ii],
-                          nStrata = settings$nstata[ii],
-                          showPlot = T,
-                          parallel = F)
+  while(current_n >= 280){
+    set.seed(iseed)
+    
+    #Create CV dataframe
+    cv = list()
+    for(spp in 1:ns) cv[[paste0('CV', spp)]] = current_CV
+    cv[['DOM']] = 1
+    cv[['domainvalue']] = 1
+    cv <- as.data.frame(cv)
+
+    #Run optimization
+    solution <- optimStrata(method = "continuous",
+                            errors = cv, 
+                            framesamp = frame,
+                            iter = 100,
+                            pops = 30,
+                            elitism_rate = 0.1,
+                            mut_chance = 1 / (istrata + 1),
+                            nStrata = istrata,
+                            showPlot = T,
+                            parallel = F)
+    
+    sum_stats = summaryStrata(solution$framenew,
+                              solution$aggr_strata,
+                              progress=FALSE) 
+    
+    #Update settings, res_df, and strata_list
+    settings = rbind(settings,
+                     data.frame(iseed = iseed,
+                                nstrata = istrata,
+                                cv = current_CV,
+                                n = sum(sum_stats$Allocation)))
+    
+    strata_list = c(strata_list, sum_stats)
+    res_df = cbind(res_df, solution$indices$X1)
+    
+    #Calculate total sample size, used to determine whether to stop 
+    #optimization or repeat the optimization with a higher CV constraint
+    current_n = sum(sum_stats$Allocation)
+    
+    #Output the results of the optimzation to the console
+    print(paste0("Just Saved: ", istrata, ' strata, ',
+                 current_CV*100, '% CV, ', current_n, 
+                 ' sample size'))
+    
+    #Update the next CV level and iseed
+    current_CV = current_CV + 0.005
+    iseed = istrata*1000 + 1
+    
+    #Plot Solution
+    goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km', 'N_km')],
+                                 data = cbind(solution$framenew[,paste0('Y',1:ns)],
+                                              Str_no = solution$framenew$STRATO,
+                                              depth = solution$framenew$X1,
+                                              lon = solution$framenew$X2) )
+    goa_ras = raster(goa, resolution = 5)
+    goa_ras =rasterize(x = goa, y = goa_ras, field = 'Str_no')
+    plot(goa_ras, col = terrain.colors(10)[-10], axes = F)
+    
+    #Save Output
+    save(list = c('res_df', 'strata_list', 'settings'),
+         file = paste0(github_dir, 'model_', VAST_model, '/optimization_', 
+                       istrata,'_strata.RData'))
+  }
   
-  strata_list[[ii]] =  summaryStrata(solution$framenew,
-                                     solution$aggr_strata,
-                                     progress=FALSE) 
-  
-  res_df = cbind(res_df, solution$framenew$STRATO)
-  
-  save(list = c('strata_list', 'res_df', 'settings', 
-                'frame', 'ns', 'NTime', 'VAST_model'), 
-       file = paste0(output_wd, '/optimization_spatiotemporal_',
-                     min(iter_range), '-', ii,'.RData') )
-  
-  #Plot
-  goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km', 'N_km')],
-                               data = cbind(solution$framenew[,paste0('Y',1:ns)],
-                                            Str_no = solution$framenew$STRATO,
-                                            depth = solution$framenew$X1,
-                                            lon = solution$framenew$X2) )
-  goa_ras = raster(goa, resolution = 5)
-  goa_ras =rasterize(x = goa, y = goa_ras, field = 'Str_no')
-  plot(goa_ras, col = terrain.colors(10)[-10], axes = F)
 }
-
-
