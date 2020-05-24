ed9f1bc42d59361b4a89c7e6d30545b22249154c
Zack Oyafuso <zackoyafuso@Zacks-MacBook-Air.local>
2020-05-24 16:34:19 -0700

    updated with 100 iterations

--- Optimum_Allocation/Stratified_RS_Simulation.R
+++ Optimum_Allocation/Stratified_RS_Simulation.R
@@ -7,9 +7,9 @@ rm(list = ls())
 ###############################
 ## Set up directories
 ###############################
-which_machine = c('Zack_MAC'=1, 'Zack_PC' =2, 'Zack_GI_PC'=3, 'VM' = 4)[2]
+which_machine = c('Zack_MAC'=1, 'Zack_PC' =2, 'Zack_GI_PC'=3, 'VM' = 4)[1]
 VAST_model = "6g"
-optimization_type = c('_spatial', '_spatiotemporal')[1]
+optimization_type = c('_spatial', '_spatiotemporal')[2]
 
 VAST_dir = paste0(c('/Users/zackoyafuso/Google Drive/', 
                     'C:/Users/Zack Oyafuso/Google Drive/', 
@@ -76,7 +76,7 @@ stmt = paste0('aggregate(cbind(',
 true_mean = eval(parse(text = stmt))[,-1]
 colnames(true_mean) = sci_names
 
-sim_mean = sim_cv = array(dim = c(NTime, ns, nrow(settings), Niters), 
+sim_mean = sim_cv = array(dim = c(NTime, ns, Nstrata, 3, Niters), 
                           dimnames = list(paste0('Year_', 1:NTime),
                                           sci_names, 
                                           NULL, 
@@ -85,77 +85,84 @@ sim_mean = sim_cv = array(dim = c(NTime, ns, nrow(settings), Niters),
 ##########################
 ## Simulating each optimization
 ##########################
-for(irow in 1:nrow(settings)) {
-   
-   print(paste0('Settings: ', settings$nstrata[irow], ' strata, ', 
-                settings$n[irow], ' samples, ', settings$cv[irow]*100, '% CV'))
-   
-   strata_allocation = strata_list[[irow]]$Allocation
-   stratapop = strata_list[[irow]]$Population
-   
-   #Good strata (with > 1 cells)
-   temp_strata = (1:length(strata_allocation))[strata_allocation >= 2]
-   
-   for(iyear in 1:NTime){
-      for(iter in 1:Niters){
-         
-         #Set Unique Seed
-         temp_seed =  getseed(temp_time = iyear, 
-                              temp_nstrata = settings$nstrata[irow], 
-                              temp_CV = settings$cv[irow], 
-                              temp_Niter = iter)
-         set.seed(temp_seed)
-         
-         #Sample based on the stratification allocations
-         sample_vec = c()
-         for(i in temp_strata ){
-            available_cells = which(res_df[,irow] == i)
-            sample_cells = sample(x = available_cells, 
-                                  size = strata_allocation[i], 
-                                  replace = F)
-            sample_vec = c(sample_vec, sample_cells)
+settings$id = 1:nrow(settings)
+
+for(istrata in 1:Nstrata){
+   for(isample in 1:3) {
+      
+      sub_settings = subset(settings, nstrata == strata[istrata])
+      
+      idx = as.integer(sub_settings$id[which.min(abs(sub_settings$n - c(280, 550, 820)[isample]))])
+      
+      strata_allocation = strata_list[[idx]]$Allocation
+      stratapop = strata_list[[idx]]$Population
+      
+      #Good strata (with > 1 cells)
+      temp_strata = (1:length(strata_allocation))[strata_allocation >= 2]
+      
+      for(iyear in 1:NTime){
+         for(iter in 1:Niters){
+            
+            #Set Unique Seed
+            temp_seed =  getseed(temp_time = iyear, 
+                                 temp_nstrata = settings$nstrata[idx], 
+                                 temp_CV = settings$cv[idx], 
+                                 temp_Niter = iter)
+            set.seed(temp_seed)
+            
+            #Sample based on the stratification allocations
+            sample_vec = c()
+            for(i in temp_strata ){
+               available_cells = which(res_df[,idx] == i)
+               sample_cells = sample(x = available_cells, 
+                                     size = strata_allocation[i], 
+                                     replace = F)
+               sample_vec = c(sample_vec, sample_cells)
+            }
+            
+            #Organize sample set and total number of samples
+            sample_vec = sort(sample_vec)
+            n = length(sample_vec)
+            stratano =  res_df[sample_vec,idx]
+            sample_df = subset(frame_raw, year == iyear)[sample_vec,]
+            
+            #Calculate Stratum Mean Density and Variance
+            stmt = paste0('aggregate(cbind(',
+                          paste0('Y', 1:(ns-1), sep = ',', collapse = ''), 'Y',ns, 
+                          ") ~ stratano, data = sample_df, FUN = mean)")
+            sample_mean = eval(parse(text = stmt))[,-1]
+            stmt = paste0('aggregate(cbind(',
+                          paste0('Y', 1:(ns-1), sep = ',', collapse = ''), 'Y',ns, 
+                          ") ~ stratano, data = sample_df, FUN = var)")
+            sample_var = eval(parse(text = stmt))[,-1]
+            
+            #How many samples are allocated in each strata
+            #How many sampling units are in each strata
+            temp_strata_allocation = strata_allocation[temp_strata] #n_h
+            temp_stratapop = stratapop[temp_strata] #N_h
+            
+            Wh = temp_stratapop/N
+            wh = temp_strata_allocation/temp_stratapop
+            
+            #Calculate Total Abundance and Variance, calculate CV
+            SRS_var = colSums(sweep(x = sample_var, MARGIN = 1, 
+                                    STATS = (Wh)^2*(1-wh)/temp_strata_allocation,
+                                    FUN = '*'))
+            
+            SRS_mean = colSums(sweep(x = sample_mean, MARGIN = 1, 
+                                     STATS = temp_stratapop / N,
+                                     FUN = '*'))
+            
+            strata_cv = sqrt(SRS_var) / SRS_mean 
+            
+            #Record mean and CV values
+            sim_mean[paste0('Year_',iyear),,istrata,isample,iter] = SRS_mean
+            sim_cv[paste0('Year_',iyear),,istrata,isample,iter] = strata_cv
+            
          }
-         
-         #Organize sample set and total number of samples
-         sample_vec = sort(sample_vec)
-         n = length(sample_vec)
-         stratano =  res_df[sample_vec,irow]
-         sample_df = subset(frame_raw, year == iyear)[sample_vec,]
-         
-         #Calculate Stratum Mean Density and Variance
-         stmt = paste0('aggregate(cbind(',
-                       paste0('Y', 1:(ns-1), sep = ',', collapse = ''), 'Y',ns, 
-                       ") ~ stratano, data = sample_df, FUN = mean)")
-         sample_mean = eval(parse(text = stmt))[,-1]
-         stmt = paste0('aggregate(cbind(',
-                       paste0('Y', 1:(ns-1), sep = ',', collapse = ''), 'Y',ns, 
-                       ") ~ stratano, data = sample_df, FUN = var)")
-         sample_var = eval(parse(text = stmt))[,-1]
-         
-         #How many samples are allocated in each strata
-         #How many sampling units are in each strata
-         temp_strata_allocation = strata_allocation[temp_strata] #n_h
-         temp_stratapop = stratapop[temp_strata] #N_h
-         
-         Wh = temp_stratapop/N
-         wh = temp_strata_allocation/temp_stratapop
-         
-         #Calculate Total Abundance and Variance, calculate CV
-         SRS_var = colSums(sweep(x = sample_var, MARGIN = 1, 
-                                 STATS = (Wh)^2*(1-wh)/temp_strata_allocation,
-                                 FUN = '*'))
-         
-         SRS_mean = colSums(sweep(x = sample_mean, MARGIN = 1, 
-                                  STATS = temp_stratapop / N,
-                                  FUN = '*'))
-         
-         strata_cv = sqrt(SRS_var) / SRS_mean 
-         
-         #Record mean and CV values
-         sim_mean[paste0('Year_', iyear), , irow,iter] = SRS_mean
-         sim_cv[paste0('Year_', iyear), , irow,iter] = strata_cv
-         
       }
+      print(paste0('Finished with: ', strata[istrata], ' Strata and ', 
+                   isample, ' Boat'))
    }
 }
 
@@ -163,36 +170,44 @@ for(irow in 1:nrow(settings)) {
 ## Simulation Metrics
 #################################
 #True CV, Cv of Cv, Rrmse of Cv
-true_cv_array = cv_cv_array = rrmse_cv_array = 
-   array(dim = c(NTime, ns, nrow(settings)), 
-         dimnames = list(paste0('Year_', 1:NTime), sci_names, NULL ))
+true_cv_array = rrmse_est_array = rrmse_cv_array = 
+   array(dim = c(NTime, ns, Nstrata, 3), 
+         dimnames = list(paste0('Year_', 1:NTime), sci_names, NULL, NULL ))
 
 for(iyear in 1:NTime){
-   for(irow in 1:nrow(settings) ){
-      for(spp in sci_names){
-         
-         iter_est = sim_mean[paste0('Year_', iyear), spp, irow, ]
-         iter_cv = sim_cv[paste0('Year_', iyear), spp, irow, ]
-         true_cv = sd(iter_est) / true_mean[iyear, spp]
-         
-         true_cv_array[paste0('Year_', iyear), spp,  irow] = true_cv
+   for(istrata in 1:Nstrata){
+      for(isample in 1:3){
+         for(spp in sci_names){
             
-         rrmse_cv_array[paste0('Year_', iyear), spp, irow] = 
-            sqrt(mean((iter_cv-true_cv)^2)) / mean(iter_cv)
+            iter_est = sim_mean[paste0('Year_', iyear),spp,istrata,isample,]
+            iter_cv = sim_cv[paste0('Year_', iyear), spp, istrata,isample, ]
+            true_cv = sd(iter_est) / true_mean[iyear, spp]
+            
+            true_cv_array[paste0('Year_', iyear),spp,istrata,isample] = true_cv
+            
+            rrmse_cv_array[paste0('Year_', iyear), spp, istrata,isample] = 
+               sqrt(mean((iter_cv-true_cv)^2)) / mean(iter_cv)
+            
+            rrmse_est_array[paste0('Year_', iyear), spp, istrata,isample] = 
+               sqrt(mean((iter_est-true_mean[iyear,spp])^2))/
+               true_mean[iyear,spp]
+         }
       }
    }
+   
 }
+
 
 #######################
 ## Save results
 #######################
-for(ivar in  c('cv_cv_array', 'rrmse_cv_array', 'true_cv_array', 
+for(ivar in  c('rrmse_est_array', 'rrmse_cv_array', 'true_cv_array', 
                'sim_mean', 'sim_cv')){
    assign(x=paste0('STRS_', ivar), value = get(ivar))
 }
 
 save(file = paste0(output_wd, '/Stratified_RS_Simulation_Results.RData'),
-     list = c(paste0('STRS_', c('cv_cv_array', 'rrmse_cv_array', 
+     list = c(paste0('STRS_', c('rrmse_est_array', 'rrmse_cv_array', 
                                 'true_cv_array', 'sim_mean', 'sim_cv')),
               'true_mean', 'sci_names', 'NTime', 'ns', 'Niters', 'N',
               'getseed'))
