a7069fceb0fa4d09b822ac508780261c6f6e3be8
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-06-07 11:16:28 -0700

    fixed error in RRMSE calculation

--- diagnostic_cross_validation_check.R
+++ diagnostic_cross_validation_check.R
@@ -5,22 +5,23 @@
 library(VAST); library(RANN)
 
 #Load Dataset
-setwd("C:/Users/zack.oyafuso/Desktop/VAST_Runs/VAST_output6f/")
+setwd("C:/Users/zack.oyafuso/Desktop/VAST_Runs/VAST_output6i/")
 
 CV_df = data.frame(ifold = 1:5)
+RRMSE = matrix(nrow = 5, ncol = 15)
 
-for(ifold in 1:5){
+for(ifold in 1:4){
   load(paste0("CV_", ifold, '/fit.RData'))
   
   # #Final Gradient
   CV_df$max_grad[ifold] = max(abs(fit_new$parameter_estimates$diagnostics$final_gradient))
   
-  
   #if hessian is positive definite, the sds are all be positive and the
   #eigenvalues of the covariance matrix are all positive
-  CV_df$all_sd_positive[ifold] = all(df$sd > 0)
+  sds = sqrt(diag(fit_new$parameter_estimates$SD$cov.fixed))
+  CV_df$all_sd_positive[ifold] = all(sds > 0)
   CV_df$all_eigen_positive[ifold] = all(eigen(fit_new$parameter_estimates$SD$cov.fixed)$values>0)
-
+  
   #check_fit chekcs bounds, TRUE is bad and FALSE is good
   CV_df$bound_check[ifold] = (check_fit(fit_new$parameter_estimates))
   
@@ -40,7 +41,7 @@ for(ifold in 1:5){
   loc_g = fit_new$spatial_list$loc_g
   
   #which grids are closest to each withheld data location 
-  grid_idx = RANN::nn2(query = withheld_loc, data = loc_g, k = 1)$nn.idx
+  grid_idx = RANN::nn2(query = withheld_df[,c('E_km', 'N_km')], data = loc_g, k = 1)$nn.idx
   
   for(irow in 1:nrow(withheld_df)) {
     withheld_df$pred_density[irow] = fit_new$Report$D_gcy[grid_idx[irow],
@@ -48,7 +49,18 @@ for(ifold in 1:5){
                                                           withheld_df$year[irow]] 
   }
   
-  CV_df$RRMSE[ifold] = with(withheld_df, 
-                            sqrt(mean((obs_density-pred_density)^2))/mean(pred_density) )
+  #RRMSE
+  mean_pred_density = aggregate(pred_density ~ spp, data = withheld_df, 
+                                FUN = mean)$pred_density
+  split_df = split.data.frame(withheld_df, f = withheld_df$spp)
+  
+  for(i in 1:15){
+    RRMSE[ifold, i] = with(split_df[[i]], 
+                             sqrt(mean((obs_density-pred_density)^2))/mean_pred_density[i] )
+  }
+  
+  
   
 }
+
+colMeans(RRMSE, na.rm = T)
