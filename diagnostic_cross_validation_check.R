6fe86ed98e45aff468c377714988b45307c2be1e
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-06-18 09:34:27 -0700

    updated RRMSE calculation

--- diagnostic_cross_validation_check.R
+++ diagnostic_cross_validation_check.R
@@ -5,10 +5,10 @@
 library(VAST); library(RANN)
 
 #Load Dataset
-setwd("C:/Users/zack.oyafuso/Desktop/VAST_Runs/VAST_output6g/")
+setwd("C:/Users/zack.oyafuso/Desktop/VAST_Runs/VAST_output8b/")
 
 CV_df = data.frame(ifold = 1:5)
-RRMSE = matrix(nrow = 5, ncol = 15)
+RRMSE = array(dim = c(5, ncol = 15, 11))
 
 for(ifold in 1:5){
   load(paste0("CV_", ifold, '/fit.RData'))
@@ -19,8 +19,7 @@ for(ifold in 1:5){
   #if hessian is positive definite, the sds are all be positive and the
   #eigenvalues of the covariance matrix are all positive
   sds = sqrt(diag(fit_new$parameter_estimates$SD$cov.fixed))
-  CV_df$all_sd_positive[ifold] = all(sds > 0)
-  CV_df$all_eigen_positive[ifold] = all(eigen(fit_new$parameter_estimates$SD$cov.fixed)$values>0)
+  CV_df$pdHess[ifold] = fit_new$parameter_estimates$SD$pdHess
   
   #check_fit chekcs bounds, TRUE is bad and FALSE is good
   CV_df$bound_check[ifold] = (check_fit(fit_new$parameter_estimates))
@@ -50,16 +49,21 @@ for(ifold in 1:5){
   }
   
   #RRMSE
-  mean_pred_density = aggregate(pred_density ~ spp, data = withheld_df, 
-                                FUN = mean)$pred_density
-  split_df = split.data.frame(withheld_df, f = withheld_df$spp)
+  mean_pred_density = aggregate(pred_density ~ spp + year, data = withheld_df, 
+                                FUN = mean)
+
   
   for(i in 1:15){
-    RRMSE[ifold, i] = with(split_df[[i]], 
-                             sqrt(mean((obs_density-pred_density)^2))/mean_pred_density[i] )
+    for(itime in 1:11){
+      split_df = subset(withheld_df, spp == i & year ==  unique(withheld_df$year)[itime])
+      
+      RRMSE[ifold, i, itime] = with(split_df, 
+                                    sqrt(mean((obs_density - pred_density)^2)) / mean(pred_density) )
+    }
+
   }
    print(paste0('Done with fold ', ifold))
 }
 
 CV_df
-colMeans(RRMSE, na.rm = T)
+round(apply(RRMSE, MARGIN = 1:2, mean), 2)
